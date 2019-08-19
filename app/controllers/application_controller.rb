ad306bed09784b5fbaf7648f26ee2bf6db396def
shadowfox3735 <normanpan180@gmail.com>
2019-08-19 15:43:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   post '/articles' do
     article = Article.create(params)
     
-    redirect to '/articles/#{article.id}'
+    redirect '/articles/#{article.id}'
   end
   
   get '/articles/:id/edit' do
